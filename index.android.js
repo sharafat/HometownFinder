6a7c5a576c9c0e9c2b0ef6bf24e77a9cff684f8d
Sharafat Ibn Mollah Mosharraf <sharafat@crypticit.com>
2016-02-20 02:37:09 +0600

    Add Friend Details

--- index.android.js
+++ index.android.js
@@ -7,11 +7,19 @@ import React, {
     AppRegistry,
     Component,
     StyleSheet,
+    Animated,
+    Easing,
+    View,
     Text,
-    View
+    Image,
+    TouchableOpacity
     } from 'react-native';
 import MapView from 'react-native-maps';
+import Animatable from 'react-native-animatable';
+import TimerMixin from 'react-timer-mixin';
 import FriendMarker from './components/FriendMarker';
+
+var { Dimensions } = require('react-native');
 
 class HometownFinder extends Component {
 
@@ -19,6 +27,10 @@ class HometownFinder extends Component {
         super(props);
 
         this.centerMapToUserLocation = this.centerMapToUserLocation.bind(this);
+        this.onMapPress = this.onMapPress.bind(this);
+        this.onMarkerPress = this.onMarkerPress.bind(this);
+        this.onSendRequest = this.onSendRequest.bind(this);
+        this.componentWillUnmount = this.componentWillUnmount.bind(this);
 
         this.state = {
             mapRegion: {
@@ -27,6 +39,8 @@ class HometownFinder extends Component {
                 latitudeDelta: 0,
                 longitudeDelta: 5
             },
+            slidingAnimationValue: new Animated.ValueXY({x: 0, y: 150}),
+            friendDetailsIsDisplayed: false,
             markers: [
                 {
                     coordinate: {
@@ -34,7 +48,9 @@ class HometownFinder extends Component {
                         longitude: 89.8343373
                     },
                     key: 0,
-                    imageUrl: "https://graph.facebook.com/334223930063686/picture"
+                    imageUrl: "https://graph.facebook.com/334223930063686/picture",
+                    name: 'Sharu',
+                    hometown: 'Manikganj'
                 },
                 {
                     coordinate: {
@@ -42,9 +58,13 @@ class HometownFinder extends Component {
                         longitude: 90.3063203
                     },
                     key: 1,
-                    imageUrl: "https://graph.facebook.com/334223930063686/picture"
+                    imageUrl: "https://graph.facebook.com/334223930063686/picture",
+                    name: 'Sadu',
+                    hometown: 'Munshiganj'
                 }
-            ]
+            ],
+            currentlyDisplayedMarker: null,
+            sendRequestButtonLabel: 'Send Chill Request'
         };
 
         this.centerMapToUserLocation();
@@ -67,37 +87,140 @@ class HometownFinder extends Component {
 
     render() {
         return (
-            <MapView style={styles.container}
-                     showsUserLocation={true}
-                     region={this.state.mapRegion}>
-                {this.state.markers.map(marker => (
-                    <MapView.Marker
-                        key={marker.key}
-                        coordinate={marker.coordinate}
-                        onPress={this.onMarkerPress.bind(this, marker.key)}>
-                        <FriendMarker imageUrl={marker.imageUrl}/>
-                    </MapView.Marker>
-                ))}
-            </MapView>
+            <View style={styles.container}>
+                <MapView style={styles.container}
+                         showsUserLocation={true}
+                         region={this.state.mapRegion}
+                         onPress={this.onMapPress}>
+                    {this.state.markers.map(marker => (
+                        <MapView.Marker
+                            key={marker.key}
+                            coordinate={marker.coordinate}
+                            onPress={this.onMarkerPress.bind(this, marker.key)}>
+                            <FriendMarker imageUrl={marker.imageUrl}/>
+                        </MapView.Marker>
+                    ))}
+                </MapView>
+
+                <Animatable.View ref="friendDetails"
+                                 style={{transform: this.state.slidingAnimationValue.getTranslateTransform()}}>
+                    <View style={styles.friendDetails}>
+                        <Image style={styles.friendPhoto}
+                               source={this.state.currentlyDisplayedMarker != null ?
+                                    {uri:  this.state.currentlyDisplayedMarker.imageUrl }
+                                    : require('image!empty_pixel')}/>
+                        <View>
+                            <Text style={styles.friendName}>
+                                {this.state.currentlyDisplayedMarker != null ? this.state.currentlyDisplayedMarker.name : ''}
+                            </Text>
+                            <Text style={styles.friendHometown}>
+                                {this.state.currentlyDisplayedMarker != null ? this.state.currentlyDisplayedMarker.hometown : ''}
+                            </Text>
+                        </View>
+                    </View>
+
+                    <TouchableOpacity onPress={this.onSendRequest}>
+                        <View style={styles.sendRequestButton}>
+                            <Text style={styles.sendRequestText}
+                                  ref="sendRequestText">
+                                {this.state.sendRequestButtonLabel}
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                </Animatable.View>
+            </View>
         );
     }
 
     onMarkerPress(markerKey) {
-        console.log(markerKey);
+        this.setState({
+            currentlyDisplayedMarker: this.state.markers[markerKey]
+        });
+
+        Animated.spring(this.state.slidingAnimationValue, {
+            tension: 0,
+            friction: 4,
+            toValue: {x: 0, y: 0}
+        }).start();
+
+        this.state.friendDetailsIsDisplayed = true;
+    }
+
+    onMapPress() {
+        if (this.state.friendDetailsIsDisplayed) {
+            Animated.spring(this.state.slidingAnimationValue, {
+                tension: 0,
+                friction: 4,
+                toValue: {x: 0, y: 150}
+            }).start();
+
+            this.state.friendDetailsIsDisplayed = false;
+        }
+    }
+
+    onSendRequest() {
+        const requestSentLabel = 'Request sent!';
+        if (this.state.sendRequestButtonLabel == requestSentLabel) {
+            return;
+        }
+
+        var sendRequestButtonLabel = this.state.sendRequestButtonLabel;
+
+        TimerMixin.setTimeout.call(this, () => {
+            this.setState({
+                sendRequestButtonLabel: requestSentLabel
+            });
+
+            TimerMixin.setTimeout.call(this, () => {
+                this.setState({
+                    sendRequestButtonLabel: sendRequestButtonLabel
+                });
+            }, 2500);
+        }, 1500);
+    }
+
+    componentWillUnmount() {
+        TimerMixin.componentWillUnmount.call(this);
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
+        flex: 1
     },
     marker: {
         width: 26,
         height: 26,
         borderRadius: 13
+    },
+    friendDetails: {
+        flexDirection: 'row',
+        backgroundColor: '#FFFFFF',
+        padding: 10
+    },
+    friendPhoto: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 10
+    },
+    friendName: {
+        fontSize: 20,
+        color: '#000000'
+    },
+    friendHometown: {
+        fontSize: 14,
+        color: '#A8A8A8'
+    },
+    sendRequestButton: {
+        height: 50,
+        backgroundColor: '#53B1EC',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    sendRequestText: {
+        fontSize: 17,
+        color: '#FFFFFF'
     }
 });
 
